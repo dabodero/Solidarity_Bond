ebe84674b975e4fb616f6189fd010411cf23adcc
Gaultier <gaultier.geelen@viacesi.fr>
2020-06-11 14:29:46 +0200

    Produit et Boutique

---
+++ Solidarity_Bond/resources/views/boutique.blade.php
@@ -0,0 +1,58 @@
+@extends('5446.Solidarity_Bond.resources.views.layouts.app')
+@section('content')
+
+
+ <div class="container text-center">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card mt-5">
+                <img class="card-img-top" src="{{ asset('assets/img/covid19-coronavirus-disease-concept-patient-600w-1671032311.png') }}" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                    <button type="button" class="btn btn-outline-primary mr-2">En savoir plus</button>
+                    <button type="button" class="btn btn-outline-primary ml-2">Ajouter au panier</button>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card mt-5">
+                <img class="card-img-top" src="{{ asset('assets/img/covid19-coronavirus-disease-concept-patient-600w-1671032311.png') }}" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                    <button type="button" class="btn btn-outline-primary mr-2">En savoir plus</button>
+                    <button type="button" class="btn btn-outline-primary ml-2">Ajouter au panier</button>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card mt-5">
+                <img class="card-img-top" src="{{ asset('assets/img/covid19-coronavirus-disease-concept-patient-600w-1671032311.png') }}" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                    <button type="button" class="btn btn-outline-primary mr-2">En savoir plus</button>
+                    <button type="button" class="btn btn-outline-primary ml-2">Ajouter au panier</button>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card mt-5">
+                <img class="card-img-top" src="{{ asset('assets/img/covid19-coronavirus-disease-concept-patient-600w-1671032311.png') }}" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                    <button type="button" class="btn btn-outline-primary mr-2">En savoir plus</button>
+                    <button type="button" class="btn btn-outline-primary ml-2">Ajouter au panier</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+@endsection
+
---
+++ Solidarity_Bond/resources/views/produit.blade.php
@@ -0,0 +1,39 @@
+@extends('5446.Solidarity_Bond.resources.views.layouts.app')
+@section('content')
+
+
+    <div class="row no-gutters">
+        <div class="col-12 col-sm-5 col-md-1 ">
+        </div>
+        <div class="col-12 col-sm-5 col-md-6  ">
+            <img class="card-img-top img-thumbnail ml-5 "
+                 src="{{ asset('assets/img/covid19-coronavirus-disease-concept-patient-600w-1671032311.png') }}"
+                 alt="Card image cap"></div>
+        <div class="col-8 col-md-4 float-left ">
+            <div class="ml-2">
+                <h4 class="ml-3 mt-3"><p>THE PRODUIT</p></h4>
+
+                <h6> Notre produit et ba franchement il est vraiment trop cool.Franchement il remplit de ouf l'est
+                    Notre produit et ba franchement il est vraiment trop cool.Franchement il remplit de ouf l'estNotre
+                    produit et ba franchement il est vraiment trop cool.Franchement il remplit de ouf l'estNotre produit
+                    et ba franchement il est vraiment trop cool.Franchement il remplit de ouf l'est
+
+                </h6>
+                <div class="prix"><h3>70$</h3></div>
+                <button type="button" class="btn btn-outline-primary ml-2 float-right ">Ajouter au panier</button>
+            </div>
+        </div>
+    </div>
+    <div class="row no-gutters">
+        <div class="col-md-6"></div>
+        <div class="col-md-5">
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Commentaire :</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                dqsdqsdqsd
+            </div>
+            <div class="col-md-1">
+            </div>
+
+        </div>
+@endsection
