1f1a651b0422feefcc78120f8a1560f2ec49b2fc
Gaultier <gaultier.geelen@viacesi.fr>
2020-06-18 12:13:10 +0200

    Affichage boutique

--- Solidarity_Bond/app/Http/Controllers/WEB/BoutiqueController.php
+++ Solidarity_Bond/app/Http/Controllers/WEB/BoutiqueController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\WEB;
 
+use App\Models\Produit;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use function GuzzleHttp\Promise\all;
 
 class BoutiqueController extends Controller
 {
     public function boutique(){
-        return view('boutique');
+       $Produits =Produit::all();
+        return view('boutique', compact('Produits'));
     }
 
     public function produit(Request $request){
