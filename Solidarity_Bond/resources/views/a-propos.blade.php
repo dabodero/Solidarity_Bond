a80dc347f7baf22ac43c39c7b3bbbd35f73ce832
Gaultier <gaultier.geelen@viacesi.fr>
2020-06-20 11:17:08 +0200

    Front A propos

--- Solidarity_Bond/resources/views/a-propos.blade.php
+++ Solidarity_Bond/resources/views/a-propos.blade.php
@@ -7,36 +7,11 @@
 @endsection
 
 @section('content')
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="{{ asset('assets/img/covid19-coronavirus-disease-concept-patient-600w-1671032311.png') }}" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="..." alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="..." alt="Third slide">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
 
-    <div class="container text-center ">
-       <h1 ><p class="titre" >Qui sommes nous ?</p></h1>
-        <h6><p>dsqdjsfdsld fshd fsdhfjkqmds hqs yhqsdljnqsmldbq sdqsj mjhsfkhdsg dKMJHQIKSDHBQKSFDSJ BSK LKQNSFKkl nbskdfk  dsqdjsfdsld fshd fsdhfjkqmds hqs yhqsdljnqsmldbq sdqsj mjhsfkhdsg dKMJHQIKSDHBQKSFDSJ BSK LKQNSFKkl nbskdfk dsqdjsfdsld fshd fsdhfjkqmds hqs yhqsdljnqsmldbq sdqsj mjhsfkhdsg dKMJHQIKSDHBQKSFDSJ BSK LKQNSFKkl nbskdfk dsqdjsfdsld fshd fsdhfjkqmds hqs yhqsdljnqsmldbq sdqsj mjhsfkhdsg dKMJHQIKSDHBQKSFDSJ BSK LKQNSFKkl nbskdfk </p></h6>
+
+    <div class="container text-center mt-5 ">
+       <h1 ><p class="titre border-bottom" >Qui sommes nous ?</p></h1>
+        <h5><p class="mt-5">Étudiants en deuxième année d'école d'ingénieurs au CESI de Lyon, nous avons décidé d'apporter notre pierre à l'édifice pour la lutte contre la propagation du COVID-19. Pour cela nous vous proposons une vitre modulable permettant de vous protéger en respectant les mesures de distantiation physique. Basé sur le campus d'écully notre école nous permet de vous offrir gracieusement nos produits.</p></h5>
     </div>
 
 
