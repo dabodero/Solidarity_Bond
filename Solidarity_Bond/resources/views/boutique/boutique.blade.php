1a85cf03f9b987da62f780fdbadfabf803fa4c29
ClappeB <bruno.clappe@viacesi.fr>
2020-06-26 15:11:43 +0200

    Correctif boutique

--- Solidarity_Bond/resources/views/boutique/boutique.blade.php
+++ Solidarity_Bond/resources/views/boutique/boutique.blade.php
@@ -20,13 +20,13 @@
             @foreach($produits as $produit)
                 <div class="col-xl-4 col-lg-6 col-sm-12 my-3">
                     <div class="card h-100">
-                <span class="span_{{$produit->ID}}" onclick="pageProduit(this.className)">
-                    <div class="card-header btn-cesi">{{$produit->Nom}}</div>
-                    @php $premierePhoto = $produit->photos()->first(); @endphp
-                    <img class="card-img-top"
-                         src="{{asset(__($premierePhoto['CheminAcces']).__($premierePhoto['Nom']))}}"
-                         alt="{{$premierePhoto['Description']}}">
-                </span>
+                        <span class="span_{{$produit->ID}}" onclick="pageProduit(this.className)">
+                            <div class="card-header btn-cesi">{{$produit->Nom}}</div>
+                            @php $premierePhoto = $produit->photos()->first(); @endphp
+                            <img class="card-img-top"
+                                 src="{{asset(__($premierePhoto['CheminAcces']).__($premierePhoto['Nom']))}}"
+                                 alt="{{$premierePhoto['Description']}}">
+                        </span>
                         <div class="card-body">
                             <h5 class="card-title">{{$produit->Nom}}</h5>
                             <div class="card-block card-size-standard">
@@ -36,30 +36,19 @@
                                     </div>
                                 </div>
                             </div>
-                            <a type="button" href="{{route('produit', ['ID_Produit' => $produit->ID])}}"
-                               class="btn btn-outline-primary mr-2">En savoir plus</a>
-                            @auth
-                                @client
-                                <button id={{$produit->ID}} type="button" class="btn btn-outline-primary ml-2"
-                                        onclick="incrementerQuantite(this.id)">Ajouter au panier
-                                </button>
-                                @endclient
-                            @endauth
-                            <input id="Nom_{{$produit->ID}}" type="text" name="Produit" value="{{$produit->Nom}}"
-                                   hidden>
+                            <div class="row m-0 p-0 justify-content-center align-items-end">
+                                <a type="button" href="{{route('produit', ['ID_Produit' => $produit->ID])}}"
+                                   class="btn btn-outline-primary mx-2">En savoir plus</a>
+                                @auth
+                                    @client
+                                    <button id={{$produit->ID}} type="button" class="btn btn-outline-primary mx-2"
+                                            onclick="incrementerQuantite(this.id)">Ajouter au panier
+                                    </button>
+                                    @endclient
+                                @endauth
+                                <input id="Nom_{{$produit->ID}}" type="text" name="Produit" value="{{$produit->Nom}}" hidden>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row m-0 p-0 justify-content-center align-items-end">
-                        <a type="button" href="{{route('produit', ['ID_Produit' => $produit->ID])}}"
-                           class="btn btn-outline-primary mx-2">En savoir plus</a>
-                        @auth
-                            @client
-                            <button id={{$produit->ID}} type="button" class="btn btn-outline-primary mx-2"
-                                    onclick="incrementerQuantite(this.id)">Ajouter au panier
-                            </button>
-                            @endclient
-                        @endauth
-                        <input id="Nom_{{$produit->ID}}" type="text" name="Produit" value="{{$produit->Nom}}" hidden>
                     </div>
                 </div>
             @endforeach
