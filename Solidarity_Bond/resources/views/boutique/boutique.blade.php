dbeb870b4e9baae57ed67c955ec02241c7bb74dd
ClappeB <bruno.clappe@viacesi.fr>
2020-06-26 12:00:49 +0200

    Revue du css boutique et du css footer

--- Solidarity_Bond/resources/views/boutique/boutique.blade.php
+++ Solidarity_Bond/resources/views/boutique/boutique.blade.php
@@ -13,13 +13,11 @@
 
 @section('content')
 
-
-
     <div class="container text-center">
-        <div class="row">
+        <div class="row mt-5">
             @foreach($produits as $produit)
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="card mt-5">
+                <div class="col-xl-4 col-lg-6 col-sm-12 my-3">
+                    <div class="card h-100">
                 <span class="span_{{$produit->ID}}" onclick="pageProduit(this.className)">
                     <div class="card-header btn-cesi">{{$produit->Nom}}</div>
                     @php $premierePhoto = $produit->photos()->first(); @endphp
@@ -48,6 +46,18 @@
                             <input id="Nom_{{$produit->ID}}" type="text" name="Produit" value="{{$produit->Nom}}"
                                    hidden>
                         </div>
+                    </div>
+                    <div class="row m-0 p-0 justify-content-center align-items-end">
+                        <a type="button" href="{{route('produit', ['ID_Produit' => $produit->ID])}}"
+                           class="btn btn-outline-primary mx-2">En savoir plus</a>
+                        @auth
+                            @client
+                            <button id={{$produit->ID}} type="button" class="btn btn-outline-primary mx-2"
+                                    onclick="incrementerQuantite(this.id)">Ajouter au panier
+                            </button>
+                            @endclient
+                        @endauth
+                        <input id="Nom_{{$produit->ID}}" type="text" name="Produit" value="{{$produit->Nom}}" hidden>
                     </div>
                 </div>
             @endforeach
